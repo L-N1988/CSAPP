5d247ad4edeee5343e8da134eefed4fed328b393
L-N1988 <2994567011@qq.com>
2022-07-01 11:49:49 +0800

    bomb lab 5/6.

---
+++ bomb/defuse.sol
@@ -0,0 +1,13 @@
+// phase_1
+Border relations with Canada have never been better.
+// phase_2
+1 2 4 8 16 32
+// phase_3 various answers
+0 207
+1 311
+2 707
+3 256
+// phase_4 (lucky one!)
+1 0
+// phase_5
+9?>567
