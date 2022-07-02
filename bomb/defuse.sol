204c2cc69ad164f4474cc97129dc90f2031902ba
L-N1988 <2994567011@qq.com>
2022-07-02 21:41:34 +0800

    bomb defused. But don't meet secrat phase\!\?

--- bomb/defuse.sol
+++ bomb/defuse.sol
@@ -9,5 +9,9 @@ Border relations with Canada have never been better.
 3 256
 // phase_4 (lucky one!)
 1 0
-// phase_5
+// phase_5 (These charaters' low 8 bits are 9/f/e/5/6/7.)
 9?>567
+// phase_6 sort ordering of: 
+// (0x603320):0x01bb/(0x603310):0x01dd/(0x603300):0x02b3/(0x6032f0):0x039c/(0x6032e0):0x00a8/(0x6032d0):0x014c
+4 3 2 1 6 5
+
